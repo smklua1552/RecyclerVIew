f3d433298796bd0cfc6f8490674c4ce774131291
smklua1552 <samuel1552davi@gmail.com>
2025-08-08 20:10:56 -0300

    Initial commit

---
+++ app/src/main/java/br/com/etecia/myapp/Produtos.java
@@ -0,0 +1,28 @@
+package br.com.etecia.myapp;
+
+public class Produtos {
+    private String titulo;
+
+    private int imagem;
+
+    public Produtos(String titulo, int imagem) {
+        this.titulo = titulo;
+        this.imagem = imagem;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getImagem() {
+        return imagem;
+    }
+
+    public void setImagem(int imagem) {
+        this.imagem = imagem;
+    }
+}
