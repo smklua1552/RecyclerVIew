7617486eca4c0809634ce96d68ed361b0ba0b533
smklua1552 <samuel1552davi@gmail.com>
2025-08-08 21:34:16 -0300

    blabaladsal

---
+++ app/src/main/java/br/com/etecia/myapp/AdaptadorProdutos.java
@@ -0,0 +1,34 @@
+package br.com.etecia.myapp;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class AdaptadorProdutos extends RecyclerView.Adapter<AdaptadorProdutos.ViewHolder> {
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
+
